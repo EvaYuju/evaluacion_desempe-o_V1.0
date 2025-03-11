3188e4b0b419caa251b88159a017258c12bc22e7
Eva <elopezmarquez@safareyes.es>
2025-03-11 23:54:51 +0100

    unified migration user + password + role

--- database/migrations/2025_03_06_004701_create_users_table.php
+++ database/migrations/2025_03_06_004701_create_users_table.php
@@ -15,24 +15,15 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->string('nif', 9);
             $table->string('sex');
             $table->foreignId('center_id')->constrained()->onDelete('cascade');
+            $table->enum('role', ['super_admin', 'admin', 'user'])->default('user');
             $table->timestamps();
         });
-    
-        // Schema::create('users', function (Blueprint $table) {
-        //     $table->id();
-        //     $table->string('name');
-        //     $table->string('email')->unique();
-        //     $table->timestamp('email_verified_at')->nullable();
-        //     $table->string('password');
-        //     $table->rememberToken();
-        //     $table->string('nif', 9);
-        //     $table->string('sex');
-        //     $table->foreignId('center_id')->constrained()->onDelete('cascade');
-        //     $table->timestamps();
-        // });
     }
 
     /**
