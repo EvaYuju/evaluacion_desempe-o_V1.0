60eaa7065f7f993b14d3f09a84a063d0b6ead8e0
Eva <elopezmarquez@safareyes.es>
2025-03-12 22:48:48 +0100

    app blade - layout 1.1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,29 +1,21 @@
- 
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title')</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <title>@yield('title', 'Gestión de Centros')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="/">Evaluación</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link" href="{{ route('users.index') }}">Usuarios</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ route('surveys.index') }}">Encuestas</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ route('questions.index') }}">Preguntas</a></li>
-                </ul>
-            </div>
+            <!--<a class="navbar-brand" href="{{ url('/') }}">Inicio</a>-->
+            <a class="navbar-brand" href="{{ route('centers.index') }}">Centros</a>
+            <a class="navbar-brand" href="{{ route('users.index') }}">Usuarios</a>
+            <a class="navbar-brand" href="{{ route('surveys.index') }}">Encuestas</a>
+            <a class="navbar-brand" href="{{ route('questions.index') }}">Preguntas</a>
         </div>
     </nav>
-
     <div class="container mt-4">
         @yield('content')
     </div>
