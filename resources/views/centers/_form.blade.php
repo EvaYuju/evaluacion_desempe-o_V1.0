3d48f2ef40815f18ea08167c024bc257a4e3a7d0
Eva <elopezmarquez@safareyes.es>
2025-03-12 22:56:47 +0100

    centros OK crud basico

--- resources/views/centers/_form.blade.php
+++ resources/views/centers/_form.blade.php
@@ -1,12 +1,15 @@
- 
 <form action="{{ $action }}" method="POST">
     @csrf
     @if($method === 'PUT')
         @method('PUT')
     @endif
     <div class="mb-3">
-        <label for="name" class="form-label">Nombre</label>
-        <input type="text" name="name" class="form-control" value="{{ old('name', $center->name ?? '') }}" required>
+        <label for="denomination" class="form-label">Denominación</label>
+        <input type="text" name="denomination" class="form-control" value="{{ old('denomination', $center->denomination ?? '') }}" required>
+    </div>
+    <div class="mb-3">
+        <label for="province" class="form-label">Provincia</label>
+        <input type="text" name="province" class="form-control" value="{{ old('province', $center->province ?? '') }}" required>
     </div>
     <button type="submit" class="btn btn-success">Guardar</button>
     <a href="{{ route('centers.index') }}" class="btn btn-secondary">Cancelar</a>
